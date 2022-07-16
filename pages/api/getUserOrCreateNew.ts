40b664f8b73a0249444c20a066f060f4b36abf4e
LimWeiJen <limweijen96@gmail.com>
2022-07-16 13:58:11 +0800

    feat: Added authentication

--- pages/api/getUserOrCreateNew.ts
+++ pages/api/getUserOrCreateNew.ts
@@ -5,17 +5,20 @@ import { PrismaClient, User } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<User>) {
-	const user = await prisma.user.findFirst({
-		where: {
-			github: req.body.github
-		}
-	})
-	if (user) return res.status(200).json(user);
-	const newUser = await prisma.user.create({
+	// find the user
+	let user = await prisma.user.findFirst({ where: { github: req.body.github } })
+
+	// if the user doesn't exist, create one
+	if (!user) user = await prisma.user.create({
 		data: {
 			name: req.body.name,
 			github: req.body.github,
 		}
 	})
-	return newUser;
+
+	// return the user
+	let data: any = user;
+	data.id = data.id.toString();
+
+	return res.status(200).json(data);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,26 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+	////// VARIABLES //////
 	const {data: session} = useSession();
 
+	////// USE EFFECTS //////
 	useEffect(() => {
+		// authentication
 		if (session) {
 			const github = session.user?.image?.replace("https://avatars.githubusercontent.com/u/", "").split("?")[0];
 			if (github) {
-				fetch('/api/getUserOrCreateNew', {
-					method: 'POST',
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify({github})
+				fetch(`https://api.github.com/user/${github}`).then(res => res.json()).then(res => {
+					fetch('/api/getUserOrCreateNew', {
+						method: 'POST',
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify({
+							github,
+							name: res.login
+						})
+					}).then(res => res.json()).then(res => {
+						console.log(res);
+					})
 				})
 			}
 		}
