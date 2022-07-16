be40357c2d0623adc6b478c237654815de5987cc
LimWeiJen <limweijen96@gmail.com>
2022-07-16 14:16:41 +0800

    feat: Added auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,5 +7,20 @@ export default NextAuth({
 			clientId: process.env.GITHUB_ID!,
 			clientSecret: process.env.GITHUB_SECRET!,
 		})
-	]
+	],
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persist the OAuth access_token to the token right after signin
+			if (account) token.accessToken = account.access_token;
+			return token
+		},
+		async session({ session, token, user }) {
+			// Send properties to the client, like an access_token from a provider.
+			const github = session.user?.image?.replace("https://avatars.githubusercontent.com/u/", "").split("?")[0];
+			const userName = await fetch(`https://api.github.com/user/${github}`).then(res => res.json()).then(res => res.login);
+			session.github = github;
+			session.userName = userName;
+			return session
+		}
+	}
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,31 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
 	////// VARIABLES //////
 	const {data: session} = useSession();
+	const [user, setUser] = useState<any>();
 
 	////// USE EFFECTS //////
 	useEffect(() => {
 		// authentication
 		if (session) {
-			const github = session.user?.image?.replace("https://avatars.githubusercontent.com/u/", "").split("?")[0];
-			if (github) {
-				fetch(`https://api.github.com/user/${github}`).then(res => res.json()).then(res => {
-					fetch('/api/getUserOrCreateNew', {
-						method: 'POST',
-						headers: {'Content-Type': 'application/json'},
-						body: JSON.stringify({
-							github,
-							name: res.login
-						})
-					}).then(res => res.json()).then(res => {
-						console.log(res);
-					})
+			fetch('/api/getUserOrCreateNew', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					github: session.github,
+					name: session.userName
 				})
-			}
+			}).then(res => res.json()).then(res => setUser(res));
 		}
 	})
 
 	return (
 		<div>
-			{session?.user?.email}
-			signed in as {session?.user?.name}
+			{user?.name}
 			<button onClick={() => signIn()}>sign in</button>
 			<button onClick={() => signOut()}>sign out</button>
 		</div>
